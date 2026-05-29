cdb5d132cce44331133cdba5928a7d9bd7320d0e
coderlawa <5d12tpff@students.codeinstitute.net>
2026-05-29 16:34:17 +0100

    model fields expanded

--- blog/models.py
+++ blog/models.py
@@ -33,7 +33,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-created_on", "author"]
+        ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
+        return f"Comment {self.body} by {self.author} on {self.post}"
